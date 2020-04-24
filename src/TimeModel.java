f3f9ef0654bca98e35fb1734aed7733adfd3afb8
MrZLeo <mrzleo@foxmail.com>
2020-04-24 21:22:41 +0800

    add bodies of start() and pause() and getTime()

--- src/TimeModel.java
+++ src/TimeModel.java
@@ -1,19 +1,43 @@
 import java.sql.Time;
 import java.util.ArrayList;
 
+import static java.lang.Thread.sleep;
+
 public class TimeModel implements TimeObservable {
     private Time time;
+    private boolean check = true;
     ArrayList<TimeObserver> timeObservers = new ArrayList<>();
 
-    public void start() {}
+    public void start() {
+        //creat a default time instance
+        start(0, 0, 0);
+    }
 
-    public void start(Time time) {}
+    public void start(int hour, int minutes, int second) {
+        //creat a time instance by static method
+        this.time.setTime(second*1000 + minutes*60*1000 + hour*60*60*1000);
+    }
 
-    public void pause() {}
+    public void pause() {
+        check = false;
+        getTime();
+    }
 
-    public void reset() {}
+    public void reset() {
+        // reset the time to the start
+        start();
+    }
 
     public Time getTime() {
+        if (check){
+            time.setTime(time.getTime()+1000);
+        }else {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         return time;
     }
 
