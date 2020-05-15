5cf95d2c188c6321f2d262ef29206aa9b22d46be
Michaelwmx <wmx_wmx@foxmail.com>
2020-05-16 01:56:50 +0800

    add little in CarTrack

--- src/model/highway/CarTrack.java
+++ src/model/highway/CarTrack.java
@@ -1,19 +1,46 @@
 package model.highway;
 
+import exception.TimeErrorException;
+import model.timer.TimeModel;
 import model.timer.TimeObserver;
 
 import java.util.ArrayList;
 
 public class CarTrack implements TimeObserver {
-    ArrayList<CarPackage> cars = new ArrayList<>();
+    private final TimeModel timeModel;
+    private final ArrayList<CarPackage> cars = new ArrayList<>();
+    private long currentTime;
+
+    CarTrack(TimeModel timeModel) {
+        this.timeModel = timeModel;
+        currentTime = 0;
+    }
 
     public void dispatchCar(BaseCar car, CarDirection direction, int location) {
         cars.add(new CarPackage(car, direction, location));
     }
 
+    private void simulateCars(long timeGap) throws TimeErrorException {
+        if (timeGap <= 0) {
+            throw new TimeErrorException();
+        } else {
+            for (CarPackage car : cars) {
+                if (car.isPullingOff) {
+
+                } else {
+
+                }
+            }
+        }
+    }
+
     @Override
     public void updateTime() {
-
+        try {
+            simulateCars(timeModel.getTime() - currentTime);
+        } catch (TimeErrorException e) {
+            e.printStackTrace();
+        }
     }
 }
 
@@ -21,10 +48,14 @@ class CarPackage {
     public BaseCar car;
     public CarDirection direction;
     public int location;
+    public boolean isPullingOff;
+    public long pullingTime;
 
     public CarPackage(BaseCar car, CarDirection direction, int location) {
         this.car = car;
         this.direction = direction;
         this.location = location;
+        isPullingOff = false;
+        pullingTime = 0;
     }
 }
