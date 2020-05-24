ebfa94ffab2ed74cd61e536454b586458343ff68
Michaelwmx <wmx_wmx@foxmail.com>
2020-05-24 17:22:21 +0800

    add Passenger

---
+++ src/passengers/Passenger.java
@@ -0,0 +1,11 @@
+package passengers;
+
+import cars.BaseCar;
+
+public class Passenger implements CarInStationObserver {
+
+    @Override
+    public void updateCarInStation(BaseCar car) {
+
+    }
+}
