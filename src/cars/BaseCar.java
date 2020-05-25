a09b7a619001f433b03c53b6ec1fdb18692b12a0
MrZLeo <mrzleo@foxmail.com>
2020-05-25 16:31:18 +0800

    Add some properties of Passenger and implement the random function that Passenger get the GetOffStation. Car can tell Passenger the station and the Passenger and decide whether get off or not by its GetOffStation

--- src/cars/BaseCar.java
+++ src/cars/BaseCar.java
@@ -6,13 +6,30 @@ import model.timer.TimeObserver;
 import passengers.CarPassengerObserver;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public abstract class BaseCar implements CarObservable, TimeObserver {
     private CarTrack track;
     private final ArrayList<CarPositionObserver> positionObservers = new ArrayList<>();
     private final ArrayList<CarPassengerObserver> passengerObservers = new ArrayList<>();
     private double speed = 0;
-
+    private final int DISTANCE_BJ = 0;
+    private final int DISTANCE_CP = 0;
+    private final int DISTANCE_GZ = 0;
+    private final int DISTANCE_WG = 0;
+    private final int DISTANCE_XP = 0;
+    private final int DISTANCE_XY = 0;
+    private final int DISTANCE_XN = 0;
+    private final Map<String, Integer> stationDistance = new TreeMap<>(){{
+        stationDistance.put("BJ", DISTANCE_BJ);
+        stationDistance.put("GZ", DISTANCE_GZ);
+        stationDistance.put("CP", DISTANCE_CP);
+        stationDistance.put("WG", DISTANCE_WG);
+        stationDistance.put("XP", DISTANCE_XP);
+        stationDistance.put("XY", DISTANCE_XY);
+        stationDistance.put("XN", DISTANCE_XN);
+    }};
     public abstract double getMaxSpeed();
     public abstract int getMaxPassengers();
     public abstract long getPullOffTime();
--- src/passengers/Passenger.java
+++ src/passengers/Passenger.java
@@ -2,7 +2,34 @@ package passengers;
 
 import cars.BaseCar;
 
+import java.util.Random;
+
 public class Passenger implements CarInStationObserver {
+    private String bordingStation;
+    private String GetOffStation;
+    private double randomGap = 16.67;
+    public Passenger(String bordingStation) {
+        this.bordingStation = bordingStation;
+        Random r = new Random();
+        double random = r.nextDouble() * 100;
+        if (random < randomGap){
+            GetOffStation = "GZ";
+        }else if (random < randomGap*2){
+            GetOffStation = "CP";
+        }else if (random < randomGap*3){
+            GetOffStation = "WG";
+        }else if (random < randomGap*4){
+            GetOffStation = "XP";
+        }else if (random < randomGap*5){
+            GetOffStation = "XY";
+        }else if (random < randomGap*6){
+            if ("BJ".equals(bordingStation)){
+                GetOffStation = "XN";
+            }else {
+                GetOffStation = "BJ";
+            }
+        }
+    }
 
     @Override
     public void updateCarInStation(BaseCar car) {
