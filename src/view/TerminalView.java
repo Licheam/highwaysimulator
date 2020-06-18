2979c980da4c5138194543459376cfd99352e9e2
MrZLeo <mrzleo@foxmail.com>
2020-06-18 22:07:27 +0800

    fix some problems in TerminalView.

--- src/view/TerminalView.java
+++ src/view/TerminalView.java
@@ -1,7 +1,12 @@
 package view;
 
+import enumerates.CarDirection;
 import enumerates.CarType;
+import exceptions.LocationErrorException;
 import model.cars.BaseCar;
+import model.cars.IvecoCar;
+import model.cars.VolveCar;
+import model.highway.CarPackage;
 import model.highway.CarPositionObserver;
 import model.highway.CarTrack;
 import model.highway.CarTrackObserver;
@@ -21,8 +26,43 @@ public class TerminalView implements TimeObserver, CarStationObserver, CarTrackO
         CarPassengerObserver, CarPositionObserver {
 
     @Override
+//  version that without CarPackage.
+//    write your code under here. :D
     public void updateCarPosition(BaseCar car) {
+    }
 
+//  version that have CarPackage.
+    public void updateCarPosition(CarTrack track) throws LocationErrorException {
+        System.out.println("正在高速路上行驶的车辆：");
+        for (CarPackage car : track.getCars()) {
+            printIDOfCar(track, car);
+            System.out.print("车辆位置：");
+            printCarLocation(track, car);
+            System.out.println("乘客数：" + car.car.getNumberOfPassengers());
+        }
+    }
+
+    private void printCarLocation(CarTrack track, CarPackage car) throws LocationErrorException {
+        String direction;
+        // BJ to XN is forward.
+        if (car.direction == CarDirection.Forward) {
+            direction = "东";
+        } else if (car.direction == CarDirection.Backward) {
+            direction = "西";
+        } else {
+            direction = "";
+        }
+        System.out.println(track.getLocationDetails(car.location, car.direction).getKey()
+                + "站" + "以" + direction
+                + track.getLocationDetails(car.location, car.direction).getValue());
+    }
+
+    private void printIDOfCar(CarTrack track, CarPackage car) {
+        if (car.car instanceof VolveCar) {
+            System.out.println("Volve:" + ((VolveCar) car.car).ID);
+        } else if (car.car instanceof IvecoCar) {
+            System.out.println("Iveco:" + ((IvecoCar) car.car).ID + car.car.getLocation());
+        }
     }
 
     @Override
