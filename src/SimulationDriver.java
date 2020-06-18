5e69a024ab4b8495c5528b0de4110368390077cc
MrZLeo <mrzleo@foxmail.com>
2020-06-18 21:49:14 +0800

    build a structure of main

--- src/SimulationDriver.java
+++ src/SimulationDriver.java
@@ -1,4 +1,8 @@
+import enumerates.CarDirection;
 import model.highway.CarTrack;
+import model.stations.BJCarStation;
+import model.stations.CarFactory;
+import model.stations.XNCarStation;
 import model.timer.TimeModel;
 import view.TerminalView;
 
@@ -7,6 +11,27 @@ public class SimulationDriver {
     public static void main(String[] args) {
         TimeModel timeModel = new TimeModel();
         TerminalView terminalView = new TerminalView();
-        CarTrack cartrack = new CarTrack(timeModel);
+        CarTrack carTrack = new CarTrack(timeModel) {{
+            addStation("BJ", 0);
+            addStation("GZ", 24);
+            addStation("CP", 45);
+            addStation("WG", 107);
+            addStation("XP", 128);
+            addStation("XY", 152);
+            addStation("XN", 174);
+        }};
+
+        timeModel.registerObserver(terminalView);
+        timeModel.registerObserver(carTrack);
+        carTrack.registerObserver(terminalView);
+
+        CarFactory carFactory = new CarFactory();
+        XNCarStation XNCarStation = new XNCarStation(carTrack, CarDirection.Backward, 174,
+                carFactory, timeModel);
+        BJCarStation BJCarStation = new BJCarStation(carTrack, CarDirection.Forward, 0,
+                carFactory, timeModel);
+
+        timeModel.start();
+        timeModel.notifyTimeObservers();
     }
 }
