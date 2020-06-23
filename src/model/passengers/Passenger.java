2bc1b62d48970fbd62d34ff817f3418a03788a67
MrZLeo <mrzleo@foxmail.com>
2020-06-23 19:31:34 +0800

    random things. Not finish yet.

--- src/model/passengers/Passenger.java
+++ src/model/passengers/Passenger.java
@@ -2,6 +2,7 @@ package model.passengers;
 
 import model.cars.BaseCar;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -13,32 +14,67 @@ import java.util.Random;
 public class Passenger implements CarInStationObserver {
     private String boardingStation;
     private String getOffStation;
-    private final double randomGap = 16.67;
+    private static int P_GZ;
+    private static int P_CP;
+    private static int P_WG;
+    private static int P_XP;
+    private static int P_XY;
+    private static int P_EndStation;
+    private static final ArrayList<String> CARSTATION = new ArrayList<>(){{
+        add("GZ");
+        add("CP");
+        add("WG");
+        add("XP");
+        add("XY");
+        add("XN");
+        add("BJ");
+    }};
 
     public Passenger(String boardingStation) {
         this.boardingStation = boardingStation;
-        Random r = new Random();
-        double random = r.nextDouble() * 100;
+        P_GZ = P_CP = P_WG = P_XP = P_XY = P_EndStation = 20;
+        getOffStation = getGetOffStation();
+    }
 
-        if (random < randomGap) {
+    private String getGetOffStation() {
+        Random r = new Random();
+        double random = r.nextDouble() * 120;
+
+        if (random < P_GZ) {
             getOffStation = "GZ";
-        } else if (random < randomGap * 2) {
+        } else if (random < P_GZ + P_CP) {
             getOffStation = "CP";
-        } else if (random < randomGap * 3) {
+        } else if (random < P_GZ + P_CP + P_WG) {
             getOffStation = "WG";
-        } else if (random < randomGap * 4) {
+        } else if (random < P_GZ + P_CP + P_WG + P_XP) {
             getOffStation = "XP";
-        } else if (random < randomGap * 5) {
+        } else if (random < P_GZ + P_CP + P_WG + P_XP + P_XY) {
             getOffStation = "XY";
-        } else if (random < randomGap * 6) {
+        } else if (random < P_GZ + P_CP + P_WG + P_XP + P_XY + P_EndStation) {
             if ("BJ".equals(this.boardingStation)) {
                 getOffStation = "XN";
             } else {
                 getOffStation = "BJ";
             }
         }
+
+        return getOffStation;
     }
 
+    public void changeProbability(String station, int probability) {
+        assert CARSTATION.contains(station);
+        assert probability <= 120;
+
+        switch (station) {
+            case "GZ" -> P_GZ = probability;
+            case "CP" -> P_CP = probability;
+            case "WG" -> P_WG = probability;
+            case "XP" -> P_XP = probability;
+            case "XY" -> P_XY = probability;
+            case "XN", "BJ" -> P_EndStation = probability;
+        }
+
+    }
 
     @Override
     public void updateCarInStation(BaseCar car, String carStation) {
