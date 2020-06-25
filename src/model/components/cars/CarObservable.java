99623940b31d7e19010c162c173cbeab385ac85b
Michaelwmx <wmx_wmx@foxmail.com>
2020-06-25 12:30:35 +0800

    split CarObservable

--- src/model/components/cars/CarObservable.java
+++ src/model/components/cars/CarObservable.java
@@ -1,19 +1,5 @@
 package model.components.cars;
 
-import enumerates.CarDirection;
-import exceptions.LocationErrorException;
-import model.components.highway.CarPositionObserver;
-import model.components.passengers.CarInStationObserver;
-import model.components.passengers.CarPassengerObserver;
+public interface CarObservable extends CarPositionObservable, CarPassengerObservable, CarInStationObservable {
 
-public interface CarObservable {
-    void registerObserver(CarPositionObserver positionObserver);
-    void removeObserver(CarPositionObserver positionObserver);
-    void notifyPositionObservers(double location, CarDirection direction) throws LocationErrorException;
-    void registerObserver(CarPassengerObserver passengerObserver);
-    void removeObserver(CarPassengerObserver passengerObserver);
-    void notifyPassengerObservers();
-    void registerObserver(CarInStationObserver inStationObserver);
-    void removeObserver(CarInStationObserver inStationObserver);
-    void notifyCarInStationObservers(String carStation);
 }
